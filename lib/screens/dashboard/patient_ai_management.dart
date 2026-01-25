ce8258692de502188a25109d6fec3b7c06e09eb1
taha <tahairtaza121@gmail.com>
2026-01-26 03:08:17 +0500

    feat: implement ComingSoonWidget and update PatientAiManagement screen

--- lib/screens/dashboard/patient_ai_management.dart
+++ lib/screens/dashboard/patient_ai_management.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/comming_soon_widget.dart';
+
 class PatientAiManagement extends StatelessWidget {
   static const String routeName = '/patient-ai-management';
   const PatientAiManagement({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const ComingSoonWidget();
   }
 }
