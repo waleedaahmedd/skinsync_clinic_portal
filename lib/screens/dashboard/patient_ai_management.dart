d986f0540bd3fc97f8c298223589344191ab8d79
tauqeerkhattak <tauqeer745@outlook.com>
2026-01-26 12:07:36 +0500

    refactor - added placeholder and updated build file

--- lib/screens/dashboard/patient_ai_management.dart
+++ lib/screens/dashboard/patient_ai_management.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/custom_fonts.dart';
+
 class PatientAiManagement extends StatelessWidget {
   static const String routeName = '/patient-ai-management';
   const PatientAiManagement({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(child: Text('Coming Soon!', style: CustomFonts.black22w600)),
+    );
   }
 }
