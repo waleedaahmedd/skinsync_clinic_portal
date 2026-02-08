3ff7b186f6818f09aef4293a4544d2981e236885
WALEED AHMED <waleed.ahmed@appedology.com>
2026-02-08 18:08:44 +0500

    refactor: improve adaptive layout widgets
    This commit enhances the responsive layout helpers.
    
    Key changes include:
    - Fixed a bug in `AdaptiveLayoutRowColumn` where children were not being added if `expandedWidget` was false.
    - Introduced `AdaptiveLayoutListInverse`, a new widget that renders a `ListView` which scrolls vertically in landscape and horizontally in portrait mode.

--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -40,7 +40,11 @@ class AdaptiveLayoutRowColumn extends StatelessWidget {
       final rowChildren = <Widget>[];
       for (var i = 0; i < children.length; i++) {
         if (i > 0) rowChildren.add(SizedBox(width: widthBetween ?? 20.w));
-        if(expandedWidget == true) rowChildren.add(Expanded(child: children[i]));
+        if (expandedWidget == true) {
+          rowChildren.add(Expanded(child: children[i]));
+        } else {
+          rowChildren.add(children[i]);
+        }
       }
       return Row(
         mainAxisAlignment: alignment ?? MainAxisAlignment.start,
@@ -99,6 +103,48 @@ class AdaptiveLayoutList extends StatelessWidget {
           return context.isLandscape
               ? SizedBox(width: spaceWidth ?? 20.w)
               : SizedBox(height: spaceHeight ?? 20.h);
+        },
+      ),
+    );
+  }
+}
+
+class AdaptiveLayoutListInverse extends StatelessWidget {
+  final List<Widget> children;
+  final double? horizontalHeight;
+
+  final double? spaceHeight;
+  final double? spaceWidth;
+
+  final bool isScrollVertical;
+
+  const AdaptiveLayoutListInverse({
+    super.key,
+    required this.children,
+    this.horizontalHeight,
+    this.spaceHeight,
+    this.spaceWidth,
+    required this.isScrollVertical,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.isLandscape ? null : horizontalHeight,
+      child: ListView.separated(
+        shrinkWrap: true,
+        itemCount: children.length,
+        physics: context.isLandscape
+            ? (isScrollVertical ? NeverScrollableScrollPhysics() : null)
+            : null,
+        scrollDirection: context.isLandscape ? Axis.vertical : Axis.horizontal,
+        itemBuilder: (context, index) {
+          return children[index];
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return context.isLandscape
+              ? SizedBox(height: spaceHeight ?? 20.h)
+              : SizedBox(width: spaceWidth ?? 20.w);
         },
       ),
     );
