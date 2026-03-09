38271ed1b25b4faa398293368553d60e0f8d2249
tauqeerkhattak <tauqeer745@outlook.com>
2026-03-09 20:46:40 +0500

    chore: initialize Firebase with web configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app_init.dart';
+import 'firebase_options.dart';
 import 'services/locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await initializeServices();
   runApp(ProviderScope(child: AppInit()));
