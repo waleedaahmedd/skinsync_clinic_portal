bb26daa2b343b90027787e1b4dee1ba51a7abd1b
saboor289 <syedsuboorhaiderjillani@gmail.com>
2026-03-12 22:00:16 +0500

    feat: implement feature-based permission assignment in custom role dialog

--- lib/models/dummy/roles.dart
+++ lib/models/dummy/roles.dart
@@ -1,2 +1,50 @@
 final List<String> modules = ['Doctors', 'Staff', 'Clinics', 'Inventory'];
+final List<Feature> feature = [
+  Feature(
+    featureId: 1,
+    featureName: 'Doctor',
+    actions: [
+      Action(actionId: 1, actionName: "Create"),
+      Action(actionId: 2, actionName: "Update"),
+      Action(actionId: 3, actionName: "Delete"),
+    ],
+  ),
+  Feature(
+    featureId: 1,
+    featureName: 'Staff',
+    actions: [
+      Action(actionId: 4, actionName: "View"),
+      Action(actionId: 5, actionName: "Edit"),
+      Action(actionId: 6, actionName: "Delete"),
+    ],
+  ),
+  Feature(
+    featureId: 1,
+    featureName: 'Clinic',
+    actions: [
+      Action(actionId: 7, actionName: "Create"),
+      Action(actionId: 8, actionName: "Update"),
+      Action(actionId: 9, actionName: "Delete"),
+      Action(actionId: 10, actionName: "View"),
+    ],
+  ),
+];
 final List<String> permissionsList = ['Create', 'Update', 'Delete'];
+
+class Feature {
+  int? featureId;
+  String? featureName;
+  List<Action>? actions;
+
+  Feature({
+    required this.featureId,
+    required this.featureName,
+    required this.actions,
+  });
+}
+
+class Action {
+  final String actionName;
+  final int actionId;
+  Action({required this.actionId, required this.actionName});
+}
--- lib/widgets/dailog box/add_custom_role_dialog.dart
+++ lib/widgets/dailog box/add_custom_role_dialog.dart
@@ -16,10 +16,20 @@ class AddCustomRoleDialog extends StatefulWidget {
 
 class _AddCustomRoleDialogState extends State<AddCustomRoleDialog> {
   final TextEditingController controller = TextEditingController();
-  Map<String, Map<String, bool>> tempPermissions = {
-    for (var module in modules)
-      module: {for (var permission in permissionsList) permission: false},
-  };
+
+  List<Feature> selectedFeatures = [];
+
+  Feature? getSelectedFeature(int featureId) {
+  try {
+    return selectedFeatures.firstWhere((e) => e.featureId == featureId);
+  } catch (e) {
+    return null;
+  }
+}
+  // Map<String, Map<String, bool>> tempPermissions = {
+  //   for (var module in modules)
+  //     module: {for (var permission in permissionsList) permission: false},
+  // };
 
   @override
   void dispose() {
@@ -77,89 +87,115 @@ class _AddCustomRoleDialogState extends State<AddCustomRoleDialog> {
               SizedBox(height: 30.h),
               Text("Assign Permissions", style: CustomFonts.black14w500),
               SizedBox(height: 16.h),
-              Table(
-                columnWidths: const {
-                  0: FlexColumnWidth(2),
-                  1: FlexColumnWidth(1),
-                  2: FlexColumnWidth(1),
-                  3: FlexColumnWidth(1),
-                },
-                children: [
-                  TableRow(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade50,
-                      border: Border(
-                        bottom: BorderSide(color: Colors.grey.shade300),
-                      ),
-                    ),
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 12.h,
-                          horizontal: 8.w,
-                        ),
-                        child: Text("Module", style: CustomFonts.black14w700),
-                      ),
-                      for (var permission in permissionsList)
-                        Center(
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 12.h),
-                            child: Text(
-                              permission,
-                              style: CustomFonts.black14w700,
-                            ),
-                          ),
-                        ),
-                    ],
-                  ),
-                  for (var module in modules)
-                    TableRow(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(color: Colors.grey.shade100),
-                        ),
-                      ),
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 16.h,
-                            horizontal: 8.w,
-                          ),
-                          child: Text(module, style: CustomFonts.black14w600),
-                        ),
-                        for (var permission in permissionsList)
-                          Center(
-                            child: Checkbox(
-                              activeColor: CustomColors.blueColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(4.r),
-                              ),
-                              value: tempPermissions[module]![permission],
-                              onChanged: (val) {
-                                setState(() {
-                                  tempPermissions[module]![permission] =
-                                      val ?? false;
-                                });
-                              },
-                            ),
-                          ),
-                      ],
-                    ),
-                ],
-              ),
+
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: feature.length,
+                itemBuilder: (context,index){
+                return Padding(
+                  padding:  EdgeInsets.only(bottom:  10.h),
+                  child: _buildFeatureContainer(feature: feature[index]),
+                );
+              }),
+              //  modules[1].actions!((sideArea) {
+                        //   return ChoiceChip(
+                        //     label: Text(sideArea.name ?? 'N/A'),
+                        //     selected: false,
+                        //     selectedColor: Colors.blue,
+                        //     checkmarkColor: Colors.white,
+                        //     labelStyle: TextStyle(
+                        //       color: Colors.black,
+                        //       fontSize: 13.sp,
+                        //       fontWeight: FontWeight.w500,
+                        //     ),
+                        //     onSelected: (_) {},
+                        //   );
+                        // }).toList(),
+              // Table(
+              //   columnWidths: const {
+              //     0: FlexColumnWidth(2),
+              //     1: FlexColumnWidth(1),
+              //     2: FlexColumnWidth(1),
+              //     3: FlexColumnWidth(1),
+              //   },
+              //   children: [
+              //     TableRow(
+              //       decoration: BoxDecoration(
+              //         color: Colors.grey.shade50,
+              //         border: Border(
+              //           bottom: BorderSide(color: Colors.grey.shade300),
+              //         ),
+              //       ),
+              //       children: [
+              //         Padding(
+              //           padding: EdgeInsets.symmetric(
+              //             vertical: 12.h,
+              //             horizontal: 8.w,
+              //           ),
+              //           child: Text("Module", style: CustomFonts.black14w700),
+              //         ),
+              //         for (var permission in permissionsList)
+              //           Center(
+              //             child: Padding(
+              //               padding: EdgeInsets.symmetric(vertical: 12.h),
+              //               child: Text(
+              //                 permission,
+              //                 style: CustomFonts.black14w700,
+              //               ),
+              //             ),
+              //           ),
+              //       ],
+              //     ),
+              //     for (var module in modules)
+              //       TableRow(
+              //         decoration: BoxDecoration(
+              //           border: Border(
+              //             bottom: BorderSide(color: Colors.grey.shade100),
+              //           ),
+              //         ),
+              //         children: [
+              //           Padding(
+              //             padding: EdgeInsets.symmetric(
+              //               vertical: 16.h,
+              //               horizontal: 8.w,
+              //             ),
+              //             child: Text(module, style: CustomFonts.black14w600),
+              //           ),
+              //           for (var permission in permissionsList)
+              //             Center(
+              //               child: Checkbox(
+              //                 activeColor: CustomColors.blueColor,
+              //                 shape: RoundedRectangleBorder(
+              //                   borderRadius: BorderRadius.circular(4.r),
+              //                 ),
+              //                 value: tempPermissions[module]![permission],
+              //                 onChanged: (val) {
+              //                   setState(() {
+              //                     tempPermissions[module]![permission] =
+              //                         val ?? false;
+              //                   });
+              //                 },
+              //               ),
+              //             ),
+              //         ],
+              //       ),
+              //   ],
+              // ),
+           
               SizedBox(height: 32.h),
               Row(
                 children: [
                   Expanded(
                     child: ElevatedButton(
                       onPressed: () {
-                        if (controller.text.trim().isNotEmpty) {
-                          widget.onAddRole(
-                            controller.text.trim(),
-                            tempPermissions,
-                          );
-                          Navigator.pop(context);
-                        }
+                        // if (controller.text.trim().isNotEmpty) {
+                        //   widget.onAddRole(
+                        //     controller.text.trim(),
+                        //     tempPermissions,
+                        //   );
+                        //   Navigator.pop(context);
+                        // }
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.black,
@@ -195,4 +231,83 @@ class _AddCustomRoleDialogState extends State<AddCustomRoleDialog> {
       ),
     );
   }
+
+Widget _buildFeatureContainer({required Feature feature}) {
+  return Container(
+    width: double.infinity,
+    padding: EdgeInsets.all(10.w),
+    decoration: BoxDecoration(
+      border: Border.all(color: CustomColors.borderColor),
+      borderRadius: BorderRadius.circular(12.r),
+      color: Colors.white,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          feature.featureName ?? "N/A",
+          style: CustomFonts.black16w600,
+        ),
+        SizedBox(height: 10.h),
+
+        Wrap(
+          spacing: 8.w,
+          runSpacing: 8.h,
+          children: List.generate(feature.actions!.length, (index) {
+            final action = feature.actions![index];
+
+            final selectedFeature = getSelectedFeature(feature.featureId!);
+
+            final isSelected =
+                selectedFeature?.actions
+                    ?.any((a) => a.actionId == action.actionId) ??
+                false;
+
+            return ChoiceChip(
+              label: Text(action.actionName),
+              selected: isSelected,
+              selectedColor: Colors.black,
+              checkmarkColor: Colors.white,
+              labelStyle: TextStyle(
+                color: isSelected ? Colors.white : Colors.black,
+                fontSize: 13.sp,
+                fontWeight: FontWeight.w500,
+              ),
+              onSelected: (value) {
+                setState(() {
+                  Feature? featureItem =
+                      getSelectedFeature(feature.featureId!);
+
+                  if (value) {
+                    if (featureItem == null) {
+                      selectedFeatures.add(
+                        Feature(
+                          featureId: feature.featureId,
+                          featureName: feature.featureName,
+                          actions: [action],
+                        ),
+                      );
+                    } else {
+                      featureItem.actions ??= [];
+                      featureItem.actions!.add(action);
+                    }
+                  } else {
+                    if (featureItem != null) {
+                      featureItem.actions!
+                          .removeWhere((a) => a.actionId == action.actionId);
+
+                      if (featureItem.actions!.isEmpty) {
+                        selectedFeatures.remove(featureItem);
+                      }
+                    }
+                  }
+                });
+              },
+            );
+          }),
+        ),
+      ],
+    ),
+  );
+}
 }
